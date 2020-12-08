08c5f33d0e580f51ec434a0ae522c79387864116
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2020-12-08 11:11:39 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static int firstNumber;
+        static int secondNumber;
+        static int result = 0;
+        static char operatorChar;
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Pls enter 1st number:");
+                firstNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Pls enter 2nd nubmer:");
+                secondNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Pls select the operation to be performed('+' , '-', '*', '/')");
+                operatorChar = char.Parse(Console.ReadLine());
+                switch (operatorChar)
+                {
+                    case '+':
+                        result = firstNumber + secondNumber;
+                        break;
+                    case '-':
+                        result = firstNumber - secondNumber;
+                        break;
+                    case '*':
+                        result = firstNumber * secondNumber;
+                        break;
+                    case '/':
+                        result = firstNumber / secondNumber;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Your input was not integer");
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("This is outside the range of Integers values");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine($"{firstNumber} {operatorChar} {secondNumber} = {result}");
+            }
+        }
+    }
+}
---
+++ ExceptionHandelingDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandelingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int firstNmbr = 0;
+            int secNmbr = 0;
+
+            try
+            {
+                Console.WriteLine("File log is open for writing");
+                Console.WriteLine("Enter first nmbr");
+                firstNmbr = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second nmbr");
+                secNmbr = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine(e.Source);
+                Console.WriteLine("*****There went something wrong:\n");
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("File log is closed");
+            }
+
+            result = firstNmbr + secNmbr;
+            Console.WriteLine(result);
+        }
+    }
+}
